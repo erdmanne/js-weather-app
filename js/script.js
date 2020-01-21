68a80fd409076e14c9c89107083be60e35aeb98d
Eva Erdmann <erdmanne@mediamarktsaturn.com>
2020-01-21 14:04:40 +0100

    add & display icon, temperature, und weather with words
    add city via var

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+var city = "Munich"
+
 //get the API from openweathermap & with console.log checking if data is loaded
-jQuery.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=0595215f568d90110031d71a7b16a440",
+jQuery.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=0595215f568d90110031d71a7b16a440",
 function(data){
   console.log(data);
 
@@ -8,7 +10,9 @@ function(data){
   var icon = "http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png";
   var temp = Math.floor(data.main.temp); //get the temperatur displayed like the icon
   var weather = data.weather[0].main // get the weather with words displayed
+
   jQuery(".icon").attr('src', icon);
   jQuery(".temp").append(temp);
   jQuery(".weather").append(weather);
+
 });
