1f2461bb51fc6fb5c745a9303818bc737c70401f
Eva Erdmann <erdmanne@mediamarktsaturn.com>
2020-01-21 14:05:58 +0100

    add comments for rounding
    Math.floor

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function(data){
 //now get the default icons which openweathermap is giving me displayed - created therefore in the indey.html the class for it
 
   var icon = "http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png";
-  var temp = Math.floor(data.main.temp); //get the temperatur displayed like the icon
+  var temp = Math.floor(data.main.temp); //get the temperatur displayed like the icon & hint: Math.floor ist für die Rundung
   var weather = data.weather[0].main // get the weather with words displayed
 
   jQuery(".icon").attr('src', icon);
